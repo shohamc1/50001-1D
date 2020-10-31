4fd87907c373bbd750c4a139b7c31dd5001e20bb
shohamc1 <shhmchk@gmail.com>
2020-11-01 00:42:42 +0800

    landing layout #2

--- app/app/src/main/java/com/shohamc1/foodavenue/MainActivity.java
+++ app/app/src/main/java/com/shohamc1/foodavenue/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,15 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
