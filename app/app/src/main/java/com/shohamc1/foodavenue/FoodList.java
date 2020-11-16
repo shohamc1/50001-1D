7b0d82e6a279a30769f117ff6b2e3e78144b9b36
qcma1 <73383356+qcma1@users.noreply.github.com>
2020-11-16 22:41:00 +0800

    Added in some code for dynamically populating the dish list under the FoodList Class.

--- app/app/src/main/java/com/shohamc1/foodavenue/FoodList.java
+++ app/app/src/main/java/com/shohamc1/foodavenue/FoodList.java
@@ -1,4 +1,30 @@
 package com.shohamc1.foodavenue;
 
-public class FoodList {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.appcompat.widget.Toolbar;
+
+public class FoodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dish_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+    LinearLayout scroll = (LinearLayout) findViewById(R.id.dishlist);
+    LayoutInflater inflater =  (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View view = inflater.inflate(R.layout.food_card, null);
+    scroll.addView(view);
+
+    @Override
+    protected void
+
+
 }
--- app/app/src/main/java/com/shohamc1/foodavenue/Landing.java
+++ app/app/src/main/java/com/shohamc1/foodavenue/Landing.java
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class Landing extends Fragment {
+public class Landing extends MainActivity {
 
     @Override
-    public View onCreateView(
+    public View onCreate(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.landing, container, false);
     }
-
+/*
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
-
+*/
     public void change_layout(android.view.View view) {
         //setContentView(R.layout.dish_list);
     }
