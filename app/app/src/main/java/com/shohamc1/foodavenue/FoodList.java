688a1428ae1e28623859b3692bce68273f8d3ba5
dksath <72483382+dksath@users.noreply.github.com>
2020-11-17 13:14:38 +0800

    create card method

--- app/app/src/main/java/com/shohamc1/foodavenue/FoodList.java
+++ app/app/src/main/java/com/shohamc1/foodavenue/FoodList.java
@@ -5,26 +5,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
-public class FoodList extends AppCompatActivity {
+import com.google.api.Distribution;
 
-    @Override
+public class FoodList extends MainActivity {
+
+
+    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    LinearLayout dishlist = findViewById(R.id.dishlist);
+    LinearLayout food_card = findViewById(R.id.food_card);
+    TextView text = findViewById(R.id.foodtext);
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dish_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        //for no.of food items
+        //i.create_card();
 
-    LinearLayout scroll = (LinearLayout) findViewById(R.id.dishlist);
-    LayoutInflater inflater =  (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    View view = inflater.inflate(R.layout.food_card, null);
-    scroll.addView(view);
 
-    @Override
-    protected void
+    }
+
+    protected void create_card(String name){
+        food_card.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        View item = getLayoutInflater().inflate(R.layout.food_card, null, false);
+        food_card.addView(item);
+        dishlist.addView(food_card);
+        //text.setText(food in database);
+
+    }
 
 
 }
+
+
+
+
+
